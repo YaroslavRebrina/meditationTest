2682116c9486617d420cc4ff8fc8758fa1c57b47
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-03-14 17:29:59 +0200

    chanching music

---
+++ src/_swiper.js
@@ -0,0 +1,8 @@
+import Swiper from 'swiper';
+import 'swiper/swiper.min.css';
+
+const swiper = new Swiper('.music__wrapper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: false,
+});
