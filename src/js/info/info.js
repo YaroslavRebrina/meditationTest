e6199a176b32f41688b6563c90ea4709510a5760
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-03-17 12:11:28 +0200

    color picker is now working

---
+++ src/js/info/info.js
@@ -0,0 +1,1 @@
+const infoBtn = document.querySelector('.button__info');
--- src/js/theme_coloring/index.js
+++ src/js/theme_coloring/index.js
@@ -1,27 +1,62 @@
 const refs = {
   colorsBtnEl: document.querySelector('.button__color'),
-  containerEl: document.querySelector('.container'),
+  wrapperEl: document.querySelector('.wrapper'),
   colorsListEl: document.querySelector('.theme__list'),
-  //   sessionBtnPallete: document.querySelector('.theme__session-palette'),
-  //   bgcPallete: document.querySelector('.theme__bgc-palette'),
+  sessionButtonEl: document.querySelector('.button__session'),
+  // sessionBtnPallete: document.querySelector('.theme__session-palette'),
+  // bgcPallete: document.querySelector('.theme__bgc-palette'),
 };
 
-const { colorsBtnEl, colorsListEl, containerEl } = refs;
+const { colorsBtnEl, colorsListEl, wrapperEl, sessionButtonEl } = refs;
 
 colorsBtnEl.addEventListener('click', palleteOpen);
 
-function palleteOpen() {
-  colorsListEl.style.transform = 'translateX(-160px)';
+function palleteOpen(e) {
+  e.stopPropagation();
+  colorsListEl.style.transform = 'translateX(-190px)';
 
-  containerEl.addEventListener('click', palleteClose);
+  wrapperEl.addEventListener('click', paletteClose);
+  colorsBtnEl.addEventListener('click', paletteClose);
 }
 
-// function palleteClose(event) {
-//   if (
-//     !event.target.nodeName === 'BUTTON' ||
-//     !event.target.classList.contains('.theme__list')
-//   ) {
-//     colorsListEl.style.transform = 'translateX(160px)';
-//     return containerEl.removeEventListener('click', palleteClose);
-//   }
-// }
+function paletteClose(e) {
+  validClick = e.composedPath().includes(colorsListEl);
+
+  if (validClick) {
+    return;
+  }
+
+  colorsListEl.style.transform = 'translateX(190px)';
+
+  wrapperEl.removeEventListener('click', paletteClose);
+  colorsBtnEl.removeEventListener('click', paletteClose);
+}
+
+// bg paintning
+
+colorsListEl.addEventListener('click', changeBgc);
+colorsListEl.addEventListener('click', changeSessionBtnColor);
+
+function changeSessionBtnColor(e) {
+  if (e.target.classList.contains('session-palette__button')) {
+    let btnColor = e.target.dataset.color;
+    let sessionBtnColorData = sessionButtonEl.dataset.color;
+
+    sessionButtonEl.classList.replace(`${sessionBtnColorData}`, `${btnColor}`);
+    return (sessionButtonEl.dataset.color = btnColor);
+  }
+
+  return;
+}
+
+function changeBgc(e) {
+  if (e.target.classList.contains('bgc-palette__button')) {
+    let bgcColor = e.target.dataset.color;
+    let wrapperColorData = wrapperEl.dataset.color;
+
+    wrapperEl.classList.replace(`${wrapperColorData}`, `${bgcColor}`);
+    return (wrapperEl.dataset.color = bgcColor);
+  }
+
+  return;
+}
